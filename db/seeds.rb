78b2f25328971c17fb4fe1d1719b6a548fac8e61
Tom Dyer <tdyer1@gmail.com>
2016-03-11 00:46:47 -0500

    Added a released year to the Movie model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ Movie.delete_all
 # NOTE: the create! method will create a row in the movies table.
 # In this case the create! method will take one argument, a Hash of key/values
 # pairs where the key is the column and the value is the data for this row.
-Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123)
-Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154)
-Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105)
+Movie.create!(name: 'Affliction', rating: 'R', desc: 'Little Dark', length: 123, released_year: 1997)
+Movie.create!(name: 'Mad Max', rating: 'R', desc: 'Fun, action', length: 154, released_year: 2015)
+Movie.create!(name: 'Rushmore', rating: 'PG-13', desc: 'Quirky humor', length: 105, released_year: 1998)
 
 puts "Created three Movies"
