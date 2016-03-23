ee9f0fed55a29fbd81d78314065264207583e3ae
Tom Dyer <tdyer1@gmail.com>
2016-03-23 00:44:14 -0400

    Added AR validations for movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,4 +14,22 @@
 
 class Movie < ActiveRecord::Base
   RATINGS = ['G', 'PG', 'PG-13', 'R', 'NC-17']
+
+  # validate that this movie has a name
+  validates :name, presence: true
+  # name must be between 2 and 30 characters
+  validates :name, length: {minimum: 2, maximum: 30}
+  # name must unique within a specific year!
+  validates :name, uniqueness: {scope: :released_year, message: "There already is a movie of this name, %{value}, for this year. " }
+
+  # validate that the rating must be one of values in the
+  # RATINGS constant.
+  validates :rating, inclusion: { in: RATINGS, message: "%{value} is not a MPAA rating" }
+
+  # validate that the length of the move is between 3 and 300 minutes.
+  validates :length, numericality: {greater_than: 2, less_than: 301 }
+
+  # validate the released year
+  validates :released_year, inclusion: { in: 1910..Date.today.year, message: "%{value} is not a valid release year, it must be between 1910 and #{Date.today.year}" }
+
 end
