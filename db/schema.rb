29e2202522206e9eaba6ade07f55e348ce5fbdf2
Tom Dyer <tdyer1@gmail.com>
2016-04-05 22:23:22 -0400

    Added an admin flag for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406014330) do
+ActiveRecord::Schema.define(version: 20160406021340) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160406014330) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
